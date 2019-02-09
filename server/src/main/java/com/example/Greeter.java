e4f2ed026706b7c2e4e193da8dcebfd60168967e
parth-devops <wzsiemens@gmail.com>
2019-02-09 15:01:05 +0530

    test java file

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
   //TODO: Add javadoc comment
-  public final String greet(String someone) {
+  public final String greet(String final someone) {
     return String.format("Hello, %s!", someone);
   }
 }
